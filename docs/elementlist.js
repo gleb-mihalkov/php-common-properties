cf579531e2e7f3fd738eac2cf1432f682cb8b22b
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-09-18 18:14:18 +0300

    Добавлено документацию.

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Common\\IProperties"],["c","Common\\TAssocProperties"],["c","Common\\TProperties"],["c","Common\\TPropertiesAccessors"],["c","Common\\TPropertiesArrayAccess"],["c","Common\\TPropertiesMemberAccess"]];
